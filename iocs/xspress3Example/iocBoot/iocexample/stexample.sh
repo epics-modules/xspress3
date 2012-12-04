8c45e465b8f3b900dc776cdcf62b50812798ff62
mp49 <mp49@e099a375-04f9-0310-9d5f-a741eaff62e1>
2012-12-04 16:29:25 +0000

    xspress3: updated to use new RHEL6 build of boost 1_48. This is different from my previous 1_49 build, but it seems to work ok.
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/support/xspress3@87972 e099a375-04f9-0310-9d5f-a741eaff62e1

--- iocs/xspress3Example/iocBoot/iocexample/stexample.sh
+++ iocs/xspress3Example/iocBoot/iocexample/stexample.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-BOOST=/dls_sw/work/common/boost/1.49.0
+BOOST=/dls_sw/prod/tools/RHEL6-x86_64/boost/1-48-0
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BOOST/stage/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BOOST/prefix/lib
 echo $LD_LIBRARY_PATH
 
 exec ./bin/linux-x86_64/example bin/linux-x86_64/stexample.boot
