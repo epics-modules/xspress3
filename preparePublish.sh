c0df7cdd4dd77a04bef438b89c075a3792d2495e
npr78 <npr78@e099a375-04f9-0310-9d5f-a741eaff62e1>
2013-07-09 13:06:55 +0000

    Added preparePublish.sh
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/support/xspress3@96375 e099a375-04f9-0310-9d5f-a741eaff62e1

---
+++ preparePublish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+epics=$(awk -F= '/^[:space:]*EPICS_BASE[:space:]*=/{print $2}' configure/RELEASE)
+
+for dir in configure iocs/xspress3Example/configure ; do
+    cp $epics/configure/*.Dls $dir
+    cp $epics/bin/$EPICS_HOST_ARCH/convertDlsRelease.pl $dir
+
+    sed -i '/include.*Dls[:space:]*$/s,(CONFIG),(TOP)/configure,' $dir/CONFIG $dir/RULES
+    sed -i '/convertDlsRelease/s,(EPICS_BASE_HOST_BIN),(TOP)/configure,' $dir/CONFIG.Dls
+done
