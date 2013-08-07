2b6f0e0836cf9a203d9f018b21cd4340590157a7
npr78 <npr78@e099a375-04f9-0310-9d5f-a741eaff62e1>
2013-08-07 17:34:30 +0000

    Added edits to update modules to external defaults in preparePublish.sh
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/support/xspress3@97676 e099a375-04f9-0310-9d5f-a741eaff62e1

--- preparePublish.sh
+++ preparePublish.sh
@@ -8,4 +8,7 @@ for dir in configure iocs/xspress3Example/configure ; do
 
     sed -i '/include.*Dls[:space:]*$/s,(CONFIG),(TOP)/configure,' $dir/CONFIG $dir/RULES
     sed -i '/convertDlsRelease/s,(EPICS_BASE_HOST_BIN),(TOP)/configure,' $dir/CONFIG.Dls
+    sed -i 's/AREADETECTOR/AREA_DETECTOR/g' $dir/RELEASE
 done
+
+sed -i '/^dbpf/s/"Yes")/"Enabled")/' iocs/xspress3Example/iocBoot/iocexample/stexample.src
