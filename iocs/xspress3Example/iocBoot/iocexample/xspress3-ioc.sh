b7683eee201c284aad87df2d165323f844208a84
npr78 <npr78@e099a375-04f9-0310-9d5f-a741eaff62e1>
2013-08-16 11:11:14 +0000

    Fixed BOOST definitions:     Added an rpath for the boost libraries     Removed LD_LIBRARY_PATH definition for boost from xspress3-ioc.sh     Change definition of BOOST to point to the boost installation prefix, not the source location
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/support/xspress3@98002 e099a375-04f9-0310-9d5f-a741eaff62e1

--- iocs/xspress3Example/iocBoot/iocexample/xspress3-ioc.sh
+++ iocs/xspress3Example/iocBoot/iocexample/xspress3-ioc.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-#BOOST=/dls_sw/prod/tools/RHEL6-x86_64/boost/1-48-0
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(BOOST)/prefix/lib
-echo $LD_LIBRARY_PATH
-
 exec $(INSTALL)/bin/linux-x86_64/example $(INSTALL)/bin/linux-x86_64/stexample.boot
