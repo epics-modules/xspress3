f7c802ad612089ffd53c715d0e5b7a900e8b527b
Matthew Moore <mdmoo1978@gmail.com>
2015-08-12 16:13:42 -0500

    Getting Adam's changes back iin

--- xspress3App/src/xspress3Epics.h
+++ xspress3App/src/xspress3Epics.h
@@ -85,54 +85,6 @@
 #define xsp3InvertVetoParamString           "XSP3_INVERT_VETO"
 #define xsp3DebounceParamString           "XSP3_DEBOUNCE"
 //Settings for a channel
-#define xsp3ChanMcaRoi1LlmParamString     "XSP3_CHAN_ROI1_LLM"
-#define xsp3ChanMcaRoi2LlmParamString     "XSP3_CHAN_ROI2_LLM"
-#define xsp3ChanMcaRoi3LlmParamString     "XSP3_CHAN_ROI3_LLM"
-#define xsp3ChanMcaRoi4LlmParamString     "XSP3_CHAN_ROI4_LLM"
-#define xsp3ChanMcaRoi5LlmParamString     "XSP3_CHAN_ROI5_LLM"
-#define xsp3ChanMcaRoi6LlmParamString     "XSP3_CHAN_ROI6_LLM"
-#define xsp3ChanMcaRoi7LlmParamString     "XSP3_CHAN_ROI7_LLM"
-#define xsp3ChanMcaRoi8LlmParamString     "XSP3_CHAN_ROI8_LLM"
-#define xsp3ChanMcaRoi9LlmParamString     "XSP3_CHAN_ROI9_LLM"
-#define xsp3ChanMcaRoi10LlmParamString     "XSP3_CHAN_ROI10_LLM"
-#define xsp3ChanMcaRoi11LlmParamString     "XSP3_CHAN_ROI11_LLM"
-#define xsp3ChanMcaRoi12LlmParamString     "XSP3_CHAN_ROI12_LLM"
-#define xsp3ChanMcaRoi13LlmParamString     "XSP3_CHAN_ROI13_LLM"
-#define xsp3ChanMcaRoi14LlmParamString     "XSP3_CHAN_ROI14_LLM"
-#define xsp3ChanMcaRoi15LlmParamString     "XSP3_CHAN_ROI15_LLM"
-#define xsp3ChanMcaRoi16LlmParamString     "XSP3_CHAN_ROI16_LLM"
-#define xsp3ChanMcaRoi1HlmParamString     "XSP3_CHAN_ROI1_HLM"
-#define xsp3ChanMcaRoi2HlmParamString     "XSP3_CHAN_ROI2_HLM"
-#define xsp3ChanMcaRoi3HlmParamString     "XSP3_CHAN_ROI3_HLM"
-#define xsp3ChanMcaRoi4HlmParamString     "XSP3_CHAN_ROI4_HLM"
-#define xsp3ChanMcaRoi5HlmParamString     "XSP3_CHAN_ROI5_HLM"
-#define xsp3ChanMcaRoi6HlmParamString     "XSP3_CHAN_ROI6_HLM"
-#define xsp3ChanMcaRoi7HlmParamString     "XSP3_CHAN_ROI7_HLM"
-#define xsp3ChanMcaRoi8HlmParamString     "XSP3_CHAN_ROI8_HLM"
-#define xsp3ChanMcaRoi9HlmParamString     "XSP3_CHAN_ROI9_HLM"
-#define xsp3ChanMcaRoi10HlmParamString     "XSP3_CHAN_ROI10_HLM"
-#define xsp3ChanMcaRoi11HlmParamString     "XSP3_CHAN_ROI11_HLM"
-#define xsp3ChanMcaRoi12HlmParamString     "XSP3_CHAN_ROI12_HLM"
-#define xsp3ChanMcaRoi13HlmParamString     "XSP3_CHAN_ROI13_HLM"
-#define xsp3ChanMcaRoi14HlmParamString     "XSP3_CHAN_ROI14_HLM"
-#define xsp3ChanMcaRoi15HlmParamString     "XSP3_CHAN_ROI15_HLM"
-#define xsp3ChanMcaRoi16HlmParamString     "XSP3_CHAN_ROI16_HLM"
-#define xsp3ChanMcaRoi1ParamString        "XSP3_CHAN_ROI1"
-#define xsp3ChanMcaRoi2ParamString        "XSP3_CHAN_ROI2"
-#define xsp3ChanMcaRoi3ParamString        "XSP3_CHAN_ROI3"
-#define xsp3ChanMcaRoi4ParamString        "XSP3_CHAN_ROI4"
-#define xsp3ChanMcaRoi5ParamString        "XSP3_CHAN_ROI5"
-#define xsp3ChanMcaRoi6ParamString        "XSP3_CHAN_ROI6"
-#define xsp3ChanMcaRoi7ParamString        "XSP3_CHAN_ROI7"
-#define xsp3ChanMcaRoi8ParamString        "XSP3_CHAN_ROI8"
-#define xsp3ChanMcaRoi9ParamString        "XSP3_CHAN_ROI9"
-#define xsp3ChanMcaRoi10ParamString        "XSP3_CHAN_ROI10"
-#define xsp3ChanMcaRoi11ParamString        "XSP3_CHAN_ROI11"
-#define xsp3ChanMcaRoi12ParamString        "XSP3_CHAN_ROI12"
-#define xsp3ChanMcaRoi13ParamString        "XSP3_CHAN_ROI13"
-#define xsp3ChanMcaRoi14ParamString        "XSP3_CHAN_ROI14"
-#define xsp3ChanMcaRoi15ParamString        "XSP3_CHAN_ROI15"
-#define xsp3ChanMcaRoi16ParamString        "XSP3_CHAN_ROI16"
 #define xsp3ChanSca0ParamString           "XSP3_CHAN_SCA0"
 #define xsp3ChanSca1ParamString           "XSP3_CHAN_SCA1"
 #define xsp3ChanSca2ParamString           "XSP3_CHAN_SCA2"
@@ -166,6 +118,7 @@ class Xspress3 : public ADDriver {
 
  public:
   Xspress3(const char *portName, int numChannels, int numCards, const char *baseIP, int maxFrames, int maxDriverFrames, int maxSpectra, int maxBuffers, size_t maxMemory, int debug, int simTest);
+  Xspress3(const char *portName, int numChannels);
   virtual ~Xspress3();
 
   /* These are the methods that we override from asynPortDriver */
@@ -175,28 +128,48 @@ class Xspress3 : public ADDriver {
                                     size_t nChars, size_t *nActual);
   virtual void report(FILE *fp, int details);
 
-  void dataTask(void);
+  const int checkForStopEvent(double timeout, const char *message);
+  const int waitForStartEvent(const char *message);
+  void adReportError(const char* message);
+  bool createMCAArray(size_t dims[2], NDArray *&pMCA, NDDataType_t dataType);
+  bool createSCAArray(void *&pSCA);
+  bool readFrame(double* pSCA, double* pMCAData, int frameNumber, int maxSpectra);
+  bool readFrame(u_int32_t* pSCA, u_int32_t* pMCAData, int frameNumber, int maxSpectra);
+  asynStatus setWindow(int channel, int sca, int llm, int hlm);
+  asynStatus connect(void);
+  void writeOutScas(void *&pSCA, int numChannels);
+  void setStartingParameters();
+  const NDDataType_t getDataType();
+  void getDims(size_t (&dims)[2]);
+  asynStatus checkHistBusy(int checkTimes);
+  const int getXsp3Handle() { return this->xsp3_handle_; }
+  xsp3Api *getXsp3() { return this->xsp3; }
+  void setNDArrayAttributes(NDArray *&pMCA, int frameNumber);
+  void setAcqStopParameters(bool aborted);
+  int getNumFramesToAcquire();
+  void doNDCallbacksIfRequired(NDArray *pMCA);
+  int getNumFramesRead();
+  void xspAsynPrint(int asynPrintType, const char *format, ...);
 
  private:
 
   //Put private functions here
   void checkStatus(int status, const char *function, const char *parentFunction);
-  asynStatus connect(void);
   asynStatus disconnect(void);
   asynStatus saveSettings(void);
   asynStatus restoreSettings(void);
   asynStatus checkConnected(void);
-  asynStatus setWindow(int channel, int sca, int llm, int hlm);
   asynStatus checkRoi(int channel, int roi, int llm, int hlm);
   asynStatus erase(void);
   asynStatus eraseSCAMCAROI(void);
   asynStatus checkSaveDir(const char *dirName);
   asynStatus readSCAParams(void);
-  asynStatus readDTCParams(void); 
-  asynStatus checkHistBusy(int checkTimes);
+  asynStatus readDTCParams(void);
   asynStatus setupITFG(void); 
   asynStatus mapTriggerMode(int mode, int invert_f0, int invert_veto, int debounce, int *apiMode);
   asynStatus setTriggerMode(int mode, int invert_f0, int invert_veto, int debounce );
+  void createInitialParameters();
+  bool setInitialParameters(int maxFrames, int maxDriverFrames, int numCards, int maxSpectra);
 
   //Put private static data members here
   static const epicsInt32 ctrlDisable_;
@@ -217,8 +190,6 @@ class Xspress3 : public ADDriver {
   static const epicsInt32 ADAcquireTrue_;
   
   //Put private dynamic here
-  epicsUInt32 acquiring_; 
-  epicsUInt32 running_; 
   int xsp3_handle_; 
 
   xsp3Api* xsp3;
@@ -259,55 +230,7 @@ class Xspress3 : public ADDriver {
   int xsp3TriggerParam;             
   int xsp3InvertF0Param;             
   int xsp3InvertVetoParam;             
-  int xsp3DebounceParam;             
-  int xsp3ChanMcaRoi1LlmParam;  
-  int xsp3ChanMcaRoi2LlmParam;   
-  int xsp3ChanMcaRoi3LlmParam;   
-  int xsp3ChanMcaRoi4LlmParam;  
-  int xsp3ChanMcaRoi5LlmParam;  
-  int xsp3ChanMcaRoi6LlmParam;  
-  int xsp3ChanMcaRoi7LlmParam;
-  int xsp3ChanMcaRoi8LlmParam;
-  int xsp3ChanMcaRoi9LlmParam;
-  int xsp3ChanMcaRoi10LlmParam;
-  int xsp3ChanMcaRoi11LlmParam;
-  int xsp3ChanMcaRoi12LlmParam;
-  int xsp3ChanMcaRoi13LlmParam;
-  int xsp3ChanMcaRoi14LlmParam;
-  int xsp3ChanMcaRoi15LlmParam;
-  int xsp3ChanMcaRoi16LlmParam;
-  int xsp3ChanMcaRoi1HlmParam;   
-  int xsp3ChanMcaRoi2HlmParam;   
-  int xsp3ChanMcaRoi3HlmParam;   
-  int xsp3ChanMcaRoi4HlmParam;   
-  int xsp3ChanMcaRoi5HlmParam;   
-  int xsp3ChanMcaRoi6HlmParam;   
-  int xsp3ChanMcaRoi7HlmParam;   
-  int xsp3ChanMcaRoi8HlmParam;   
-  int xsp3ChanMcaRoi9HlmParam;   
-  int xsp3ChanMcaRoi10HlmParam;   
-  int xsp3ChanMcaRoi11HlmParam;   
-  int xsp3ChanMcaRoi12HlmParam;   
-  int xsp3ChanMcaRoi13HlmParam;   
-  int xsp3ChanMcaRoi14HlmParam;   
-  int xsp3ChanMcaRoi15HlmParam;   
-  int xsp3ChanMcaRoi16HlmParam;   
-  int xsp3ChanMcaRoi1Param;      
-  int xsp3ChanMcaRoi2Param;      
-  int xsp3ChanMcaRoi3Param;      
-  int xsp3ChanMcaRoi4Param;      
-  int xsp3ChanMcaRoi5Param;      
-  int xsp3ChanMcaRoi6Param;      
-  int xsp3ChanMcaRoi7Param;      
-  int xsp3ChanMcaRoi8Param;      
-  int xsp3ChanMcaRoi9Param;      
-  int xsp3ChanMcaRoi10Param;      
-  int xsp3ChanMcaRoi11Param;      
-  int xsp3ChanMcaRoi12Param;      
-  int xsp3ChanMcaRoi13Param;      
-  int xsp3ChanMcaRoi14Param;      
-  int xsp3ChanMcaRoi15Param;      
-  int xsp3ChanMcaRoi16Param;      
+  int xsp3DebounceParam;
   int xsp3ChanSca0Param;
   int xsp3ChanSca1Param;             
   int xsp3ChanSca2Param;
