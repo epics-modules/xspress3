e9aa1b6ab74781472cea25a64c00d1199bf440da
Matthew Moore <mdmoo1978@gmail.com>
2015-08-18 12:35:29 -0500

    remove extra CRs

--- xspress3App/src/xspress3Epics.cpp
+++ xspress3App/src/xspress3Epics.cpp
@@ -1548,11 +1548,9 @@ const int Xspress3::waitForStartEvent(const char *message)
  */
 void Xspress3::writeOutScas(void *&pSCA, int numChannels, NDDataType_t dataType)
 {
-    if (dataType == NDFloat64)
-    {
+    if (dataType == NDFloat64) {
       double *pScaData = static_cast<double*>(pSCA);
-      for (int chan=0; chan<numChannels; ++chan) 
-      {
+      for (int chan=0; chan<numChannels; ++chan) {
         this->setDoubleParam(chan, this->xsp3ChanSca0Param, static_cast<epicsFloat64>(pScaData[0]));
         this->setDoubleParam(chan, this->xsp3ChanSca1Param, static_cast<epicsFloat64>(pScaData[1]));
         this->setDoubleParam(chan, this->xsp3ChanSca2Param, static_cast<epicsFloat64>(pScaData[2]));
@@ -1564,11 +1562,9 @@ void Xspress3::writeOutScas(void *&pSCA, int numChannels, NDDataType_t dataType)
         pScaData += XSP3_SW_NUM_SCALERS;
       }
     }
-    else
-    {
+    else {
       u_int32_t *pScaData = static_cast<u_int32_t*>(pSCA);
-      for (int chan=0; chan<numChannels; ++chan) 
-      {
+      for (int chan=0; chan<numChannels; ++chan) {
         this->setDoubleParam(chan, this->xsp3ChanSca0Param, static_cast<epicsFloat64>(pScaData[0]));
         this->setDoubleParam(chan, this->xsp3ChanSca1Param, static_cast<epicsFloat64>(pScaData[1]));
         this->setDoubleParam(chan, this->xsp3ChanSca2Param, static_cast<epicsFloat64>(pScaData[2]));
