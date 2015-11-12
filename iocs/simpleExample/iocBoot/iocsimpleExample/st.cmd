ffa14f3f325f5206fd62c9ceac63414a3c55cf38
Adam Bark <Adam.Bark@diamond.ac.uk>
2015-11-12 13:46:04 +0000

    Area detectorify the build system
    Edit Makefiles, configure and example iocBoot to build in a clean
    environment and follow the areaDetector style of build system.

--- iocs/simpleExample/iocBoot/iocsimpleExample/st.cmd
+++ iocs/simpleExample/iocBoot/iocsimpleExample/st.cmd
@@ -1,9 +1,7 @@
-#!$(INSTALL)/bin/$(ARCH)/simpleExample
-
-cd "$(INSTALL)"
+< envPaths
 
 ## Register all support components
-dbLoadDatabase("dbd/simpleExample.dbd")
+dbLoadDatabase("$(TOP)/dbd/simpleExample.dbd")
 simpleExample_registerRecordDeviceDriver(pdbbase)
 
 # xspress3Config(Port name, num channels, num cards, base IP address, max frames, max driver frames, max spectra, max buffers, max memory, debug, sim test)
@@ -11,6 +9,6 @@ xspress3Config("XSP3", 4, 1, "127.0.0.1", 1000, 1000, 4096, 1024, -1, 1, 1)
 
 ## Load record instances
 #dbLoadRecords("db/<filename>.db","<List of macros, e.g. user=xxx>")
-dbLoadRecords("db/simpleExample.db")
+dbLoadRecords("$(TOP)/db/simpleExample.db")
 
 iocInit()
