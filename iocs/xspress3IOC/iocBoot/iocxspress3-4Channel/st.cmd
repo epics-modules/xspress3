22e38b70735b77f3c79f057054e14a55f1120030
Matt Newville <newville@cars.uchicago.edu>
2016-05-06 18:32:41 +0100

    using new SCAROI.cmd in main startup script

--- iocs/xspress3IOC/iocBoot/iocxspress3-4Channel/st.cmd
+++ iocs/xspress3IOC/iocBoot/iocxspress3-4Channel/st.cmd
@@ -4,19 +4,13 @@ errlogInit(20000)
 # Device initialisation
 # ---------------------
 
-
-
-epicsEnvSet "EPICS_CA_MAX_ARRAY_BYTES", '6000000'
+epicsEnvSet "EPICS_CA_MAX_ARRAY_BYTES", '30000000'
 epicsEnvSet "EPICS_TS_MIN_WEST", '0'
-
 
 dbLoadDatabase("$(TOP)/dbd/xspress3App.dbd")
 xspress3App_registerRecordDeviceDriver(pdbbase) 
 
-
-
-
-epicsEnvSet("PREFIX", "dp_xsp3_1:")
+epicsEnvSet("PREFIX", "DP_XPS3:")
 epicsEnvSet("PORT",   "XSP3")
 epicsEnvSet("QSIZE",  "128")
 
@@ -34,19 +28,14 @@ epicsEnvSet("CBUFFS", "500")
 # The search path for database files
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db:$(XSPRESS3)/db:.")
 
-
-<AutoSave.cmd
-
 epicsEnvSet("IOC","xspress3IOC")
 
 
 #DevIOCStats
 dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db", "IOC=$(IOC)")
 
-
 # Loading libraries
 # -----------------
-
 
 callbackSetQueueSize(4000)
 
@@ -55,21 +44,23 @@ callbackSetQueueSize(4000)
 # Start Xspress3 driver
 
 # Parameters
-# portName The Asyn port name to use
-# numChannels The max number of channels (eg. 8)
-# numCards The number of Xspress3 systems (normally 1)
-# baseIP The base address used by the Xspress3 1Gig and 10Gig interfaces (eg. "192.168.0.1")
-# Max Frames Used for XSPRESS3 API Configuration (depends on Xspress3 firmware config).
-# Max Driver Frames Used to limit how big EPICS driver arrays are). Needs to match database MAX_FRAMES_DRIVER_RBV.
+# portName: The Asyn port name to use
+# numChannels: The max number of channels (eg. 8)
+# numCards: The number of Xspress3 systems (normally 1)
+# baseIP: The base address used by the Xspress3 
+#         1Gig and 10Gig interfaces (eg. "192.168.0.1")
+# Max Frames: Used for XSPRESS3 API Configuration 
+#             (depends on Xspress3 firmware config).
+# Max Driver Frames: Used to limit how big EPICS driver 
+#            arrays are. Needs to match database MAX_FRAMES_DRIVER_RBV.
 # maxSpectra The maximum size of each spectra (eg. 4096)
 # maxBuffers Used by asynPortDriver (set to -1 for unlimited)
 # maxMemory Used by asynPortDriver (set to -1 for unlimited)
 # debug This debug flag is passed to xsp3_config in the Xspress API (0 or 1)
 # simTest 0 or 1. Set to 1 to run up this driver in simulation mode. 
-xspress3Config("$(PORT)", "$(YSIZE)", "$(XSP3CARDS)", "192.168.0.1", "$(NCHANS)", 16384, "$(XSIZE)", -1, -1, 0, 1 )
+xspress3Config("$(PORT)", "$(YSIZE)", "$(XSP3CARDS)", "192.168.0.1", "$(NCHANS)", 16384, "$(XSIZE)", -1, -1, 0, 0)
 
-###########
-#
+############
 
 # Create a processing plugin
 NDProcessConfigure("PROC1", $(QSIZE), 0, "$(PORT)", 0, 0, 0)
@@ -85,69 +76,41 @@ dbLoadRecords("xspress3.template","P=$(PREFIX),R=det1:,PORT=$(PORT), ADDR=0, TIM
 
 #########################################
 #Channel 1
-#########################################
 epicsEnvSet("CHAN",   "1")
-dbLoadRecords("xspress3_AttrReset.template", "P=$(PREFIX),R=det1:,CHAN=$(CHAN)")
-
+epicsEnvSet("XADDR",  "0")
 <SCAROI.cmd
-dbLoadRecords("xspress3ChannelSCAThreshold.template", "P=$(PREFIX),R=C$(CHAN)SCAs:5:,PORT=$(PORT),ADDR=0,TIMEOUT=1,CHAN=$(CHAN), SCA=4")
-dbLoadRecords("xspress3ChannelSCALimits.template", "P=$(PREFIX),R=C$(CHAN)SCAs:6:,PORT=$(PORT), ADDR=0, TIMEOUT=1, CHAN=$(CHAN), SCA=5")
-dbLoadRecords("xspress3ChannelSCALimits.template", "P=$(PREFIX),R=C$(CHAN)SCAs:7:,PORT=$(PORT), ADDR=0, TIMEOUT=1, CHAN=$(CHAN), SCA=6")
-dbLoadRecords("xspress3ChannelDTC.template", "P=$(PREFIX),R=det1:,PORT=$(PORT),CHAN=$(CHAN), NDARRAY_PORT=$(PORT),ADDR=0,TIMEOUT=5")
 
 #########################################
 #Channel 2
-#########################################
 epicsEnvSet("CHAN",   "2")
-dbLoadRecords("xspress3_AttrReset.template", "P=$(PREFIX),R=det1:,CHAN=$(CHAN)")
-
+epicsEnvSet("XADDR",  "1")
 <SCAROI.cmd
-dbLoadRecords("xspress3ChannelSCAThreshold.template", "P=$(PREFIX),R=C$(CHAN)SCAs:5:,PORT=$(PORT),ADDR=1,TIMEOUT=1,CHAN=$(CHAN), SCA=4")
-dbLoadRecords("xspress3ChannelSCALimits.template", "P=$(PREFIX),R=C$(CHAN)SCAs:6:,PORT=$(PORT), ADDR=1, TIMEOUT=1, CHAN=$(CHAN), SCA=5")
-dbLoadRecords("xspress3ChannelSCALimits.template", "P=$(PREFIX),R=C$(CHAN)SCAs:7:,PORT=$(PORT), ADDR=1, TIMEOUT=1, CHAN=$(CHAN), SCA=6")
-dbLoadRecords("xspress3ChannelDTC.template", "P=$(PREFIX),R=det1:,PORT=$(PORT),CHAN=$(CHAN), NDARRAY_PORT=$(PORT),ADDR=1,TIMEOUT=5")
 
 #########################################
 #Channel 3
-#########################################
 epicsEnvSet("CHAN",   "3")
-dbLoadRecords("xspress3_AttrReset.template", "P=$(PREFIX),R=det1:,CHAN=$(CHAN)")
-
+epicsEnvSet("XADDR",  "2")
 <SCAROI.cmd
-dbLoadRecords("xspress3ChannelSCAThreshold.template", "P=$(PREFIX),R=C$(CHAN)SCAs:5:,PORT=$(PORT),ADDR=2,TIMEOUT=1,CHAN=$(CHAN), SCA=4")
-dbLoadRecords("xspress3ChannelSCALimits.template", "P=$(PREFIX),R=C$(CHAN)SCAs:6:,PORT=$(PORT), ADDR=2, TIMEOUT=1, CHAN=$(CHAN), SCA=5")
-dbLoadRecords("xspress3ChannelSCALimits.template", "P=$(PREFIX),R=C$(CHAN)SCAs:7:,PORT=$(PORT), ADDR=2, TIMEOUT=1, CHAN=$(CHAN), SCA=6")
-dbLoadRecords("xspress3ChannelDTC.template", "P=$(PREFIX),R=det1:,PORT=$(PORT),CHAN=$(CHAN), NDARRAY_PORT=$(PORT),ADDR=2,TIMEOUT=5")
 
 #########################################
 #Channel 4
-#########################################
 epicsEnvSet("CHAN",   "4")
-dbLoadRecords("xspress3_AttrReset.template", "P=$(PREFIX),R=det1:,CHAN=$(CHAN)")
-
+epicsEnvSet("XADDR",  "3")
 <SCAROI.cmd
-dbLoadRecords("xspress3ChannelSCAThreshold.template", "P=$(PREFIX),R=C$(CHAN)SCAs:5:,PORT=$(PORT),ADDR=3,TIMEOUT=1,CHAN=$(CHAN), SCA=4")
-dbLoadRecords("xspress3ChannelSCALimits.template", "P=$(PREFIX),R=C$(CHAN)SCAs:6:,PORT=$(PORT), ADDR=3, TIMEOUT=1, CHAN=$(CHAN), SCA=5")
-dbLoadRecords("xspress3ChannelSCALimits.template", "P=$(PREFIX),R=C$(CHAN)SCAs:7:,PORT=$(PORT), ADDR=3, TIMEOUT=1, CHAN=$(CHAN), SCA=6")
-dbLoadRecords("xspress3ChannelDTC.template", "P=$(PREFIX),R=det1:,PORT=$(PORT),CHAN=$(CHAN), NDARRAY_PORT=$(PORT),ADDR=3,TIMEOUT=5")
-
-
 
 # Optional: load scan records
-#dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db", "P=$(PREFIX),MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
-
+# dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db", "P=$(PREFIX),MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
 # Optional: load sseq record for acquisition sequence
-dbLoadRecords("$(CALC)/calcApp/Db/yySseq.db", "P=$(PREFIX), S=AcquireSequence")
+# dbLoadRecords("$(CALC)/calcApp/Db/yySseq.db", "P=$(PREFIX), S=AcquireSequence")
 
-
+< AutoSave.cmd
 
 #########################################
-# Final ioc initialization
-# ------------------------
+# ioc initialization
 
 iocInit
 
-<defaultValueLoad.cmd
+< defaultValueLoad.cmd
 
 # save settings every thirty seconds
 create_monitor_set("auto_settings.req",30,"P=$(PREFIX)")
