2d5c3069d69c58d70af6d691825626a8ff3fa4ac
Casper Ti. Vector <CasperVector@gmail.com>
2021-02-09 16:12:52 +0800

    Use a simpler example IOC.

---
+++ iocs/xspress3IOC/iocBoot/iocXspress3/xsp3-4ch-batch.cmd
@@ -0,0 +1,5 @@
+iocshLoad("$(TOP)/iocBoot/$(IOC)/chan-$(TASK).cmd", "CHAN=1,ADDR=0")
+iocshLoad("$(TOP)/iocBoot/$(IOC)/chan-$(TASK).cmd", "CHAN=2,ADDR=1")
+iocshLoad("$(TOP)/iocBoot/$(IOC)/chan-$(TASK).cmd", "CHAN=3,ADDR=2")
+iocshLoad("$(TOP)/iocBoot/$(IOC)/chan-$(TASK).cmd", "CHAN=4,ADDR=3")
+
