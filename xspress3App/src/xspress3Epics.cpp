dc1fce913b758305e6a4c2886e359e6127719c67
Tom Griffin <tom@quantumdetectors.com>
2023-01-19 14:48:23 +0000

    tidy up errors in itfg call

--- xspress3App/src/xspress3Epics.cpp
+++ xspress3App/src/xspress3Epics.cpp
@@ -1120,7 +1120,7 @@ asynStatus Xspress3::setTriggerMode(int mode, int invert_f0, int invert_veto, in
 				xsp3_status = xsp3->itfg_setup( xsp3_handle_, 0, num_frames,
 									(u_int32_t) floor(exposureTime*80E6+0.5),
 									XSP3_ITFG_TRIG_MODE_SOFTWARE, XSP3_ITFG_GAP_MODE_1US );
-				printf(exposureTime);
+				//printf(exposureTime);
 			mode = 1; // Refactor to 1 to continue to use the internal trigger generator but set up so won't continue until histogram_continue is called
 			}
             status = mapTriggerMode(mode, invert_f0, invert_veto, debounce, &xsp3_trigger_mode);
