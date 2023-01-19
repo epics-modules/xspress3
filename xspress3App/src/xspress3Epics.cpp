4262c28fca5e89952f2d7c9270c9c14262dee850
Tom Griffin <tom@quantumdetectors.com>
2023-01-19 16:47:26 +0000

    Using ITFGsetup2

--- xspress3App/src/xspress3Epics.cpp
+++ xspress3App/src/xspress3Epics.cpp
@@ -1204,28 +1204,30 @@ asynStatus Xspress3::writeInt32(asynUser *pasynUser, epicsInt32 value)
 	    setupITFG();
 		getIntegerParam(xsp3TriggerModeParam, &trigger_mode);
 		if (trigger_mode==7){
-			xsp3->histogram_start(xsp3_handle_,-1);
+			//xsp3->histogram_start(xsp3_handle_,-1);
+			epicsEventSignal(this->startEvent_);
 			xsp3->histogram_arm(xsp3_handle_,-1);
 			for ( test = 0 ; test < 20 ; test++){
+
 				printf("Looping through the advances %d\n", test);
 				xsp3->histogram_continue(xsp3_handle_,0);
 				sleep(2);
 				xsp3->histogram_pause(xsp3_handle_,0);
 			}
 		} else {
-	    xsp3_status = xsp3->histogram_start(xsp3_handle_, -1 );
-		}
-	    if (xsp3_status != XSP3_OK) {
-	      checkStatus(xsp3_status, "xsp3_histogram_start", functionName);
-	      status = asynError;
-	    }
-	    if (status == asynSuccess) {
-	      epicsEventSignal(this->startEvent_);
-	      asynPrint(this->pasynUserSelf, ASYN_TRACE_FLOW, "%s Started Data Collection.\n", functionName);
-	    } else {
-	      asynPrint(this->pasynUserSelf, ASYN_TRACE_FLOW, "%s Start Data Collection, failed.\n", functionName);
-	    }
-	  }
+			xsp3_status = xsp3->histogram_start(xsp3_handle_, -1 );
+			
+			if (xsp3_status != XSP3_OK) {
+			checkStatus(xsp3_status, "xsp3_histogram_start", functionName);
+			status = asynError;
+			}
+			if (status == asynSuccess) {
+			epicsEventSignal(this->startEvent_);
+			asynPrint(this->pasynUserSelf, ASYN_TRACE_FLOW, "%s Started Data Collection.\n", functionName);
+			} else {
+			asynPrint(this->pasynUserSelf, ASYN_TRACE_FLOW, "%s Start Data Collection, failed.\n", functionName);
+			}
+	  	}
 	}
       }
     } else {
