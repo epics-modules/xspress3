9f3fe474ea4c0d93ab215ec1a70e31efa80fa813
Tom Griffin <tom@quantumdetectors.com>
2023-01-20 14:22:20 +0000

    Using status

--- xspress3App/src/xspress3Epics.cpp
+++ xspress3App/src/xspress3Epics.cpp
@@ -1186,9 +1186,9 @@ asynStatus Xspress3::writeInt32(asynUser *pasynUser, epicsInt32 value)
 	getIntegerParam(xsp3FrameAdvanceParam, &xsp3_frame_advance);
 	// Frame advance parameter, 1 to start frame 1 to stop, you need to call both if using itfg mode (manual)
 	if (xsp3_frame_advance==1) {
-		status = xsp3->histogram_continue(xsp3_handle_,0);
+		xsp3_status = xsp3->histogram_continue(xsp3_handle_,0);
 	} else if (xsp3_frame_advance==0) {
-		status = xsp3->histogram_pause(xsp3_handle_,0);
+		xsp3_status = xsp3->histogram_pause(xsp3_handle_,0);
 	}
   }
   else if (function == ADAcquire) {
