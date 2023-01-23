253a116a25b06e7428f0996ed16b46a59a71a9ba
Tom Griffin <tom@quantumdetectors.com>
2023-01-23 10:00:45 +0000

    Correcting Value getting from the asyn

--- xspress3App/src/xspress3Epics.cpp
+++ xspress3App/src/xspress3Epics.cpp
@@ -1183,11 +1183,11 @@ asynStatus Xspress3::writeInt32(asynUser *pasynUser, epicsInt32 value)
     }
   }
   else if (function == xsp3FrameAdvanceParam) {
-	getIntegerParam(xsp3FrameAdvanceParam, &xsp3_frame_advance);
+
 	// Frame advance parameter, 1 to start frame 1 to stop, you need to call both if using itfg mode (manual)
-	if (xsp3_frame_advance==1) {
+	if (value==1) {
 		xsp3_status = xsp3->histogram_continue(xsp3_handle_,0);
-	} else if (xsp3_frame_advance==0) {
+	} else if (value==0) {
 		xsp3_status = xsp3->histogram_pause(xsp3_handle_,0);
 	}
   }
