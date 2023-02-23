34a0fec0d4cedc756c79374bf5d694767bca0188
Matthew Newville <newville@cars.uchicago.edu>
2023-02-23 12:50:29 -0600

    remove code from gh-pages branch

--- iocs/xspress3IOC/iocBoot/iocDualMini_4Channel/start_caQtDM_xspress3.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export EPICS_CA_MAX_ARRAY_BYTES=30000000
-source /local/DPbin/Scripts/Paths.bash
-source /local/DPbin/Scripts/caQtDM_setup.bash
-
-
-caQtDM -macro "P="$1":, R=det1:" xspress3_custom.ui &
--- iocs/xspress3IOC/iocBoot/iocxspress3-4Channel/start_ioc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export EPICS_CA_MAX_ARRAY_BYTES=30000000
-../../bin/linux-x86_64/xspress3App st.cmd
